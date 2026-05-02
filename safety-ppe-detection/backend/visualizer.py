3203746364a1feca4a5c94e34df1f2d01afa1106
BOguTEJlbHJlO <132605587+BOguTEJlbHJlO@users.noreply.github.com>
2026-05-02 11:44:14 +0300

    Delete safety-ppe-detection directory

--- safety-ppe-detection/backend/visualizer.py
+++
@@ -1,9 +0,0 @@
-class Visualizer:
-    def draw(self, frame, results):
-        """
-        Рисует:
-        - bounding boxes
-        - цвет (зелёный/красный)
-        - текст
-        """
-        pass
